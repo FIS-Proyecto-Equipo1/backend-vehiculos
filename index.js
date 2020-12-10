47292b8fa73d6d8adab2ef75b969c71c90275730
FredoFuentesUS <afuentes2510@gmail.com>
2020-12-10 19:46:01 +0000

    Added support to nedb for POST

--- index.js
+++ index.js
@@ -1,49 +1,23 @@
 var exp = require('express');
 var body_parser = require('body-parser');
+var DataStore = require('nedb');
+
 var port = 3000;
 var BASE_API_PATH = "/api/v1";
+var DB_FILE_NAME = __dirname + '/vehicles.json';
 
 console.log("Starting api server... ");
 
-var vehicles = [
-    {
-        "id":"1",
-        "tipo":"bicicleta",
-        "estado":"RESERVADO",
-        "permiso":"NO_REQ",
-        "localizacion":"av la palmera, sevilla"
-    },
-    {
-        "id":"2",
-        "tipo":"bicicleta",
-        "estado":"RESERVADO",
-        "permiso":"NO_REQ",
-        "localizacion":"av la palmera, sevilla"
-    },
-    {
-        "id":"3",
-        "tipo":"coche",
-        "estado":"DISPONIBLE",
-        "permiso":"B",
-        "localizacion":"giralda , sevilla"
-    },
-    {
-        "id":"4",
-        "tipo":"moto",
-        "estado":"DISPONIBLE",
-        "permiso":"AB",
-        "localizacion":"sevilla"
-    }
-];
-
 var app = exp();
 app.use(body_parser.json());
+var db = new DataStore({
+    filename : DB_FILE_NAME,
+    autoload : true
+});
 
 app.get("/", (req, res)  => {
     res.send("<html><body><h1>MY SERVER IS RUNNING</h1></body></html>");
 });
-
-
 
 app.get(BASE_API_PATH + "/vehicles", (req, res)  => {
     console.log(Date() + "GET/vehicles")
@@ -53,8 +27,15 @@ app.get(BASE_API_PATH + "/vehicles", (req, res)  => {
 app.post(BASE_API_PATH + "/vehicles", (req, res)  => {
     console.log(Date() + "POST /vehicles")
     var veh = req.body;
-    vehicles.push(veh);
-    res.sendStatus(201);
+    db.insert(veh, (err) => {
+        if(err)
+        {
+            console.log(Date()+" - "+ err);
+            res.send(500);
+        }else{
+            res.sendStatus(201);
+        }
+    });
 });
 
 app.listen(port);
