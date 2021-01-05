20d630ab3c07293412778534dec1b528228e64b5
Alfredo Fuentes <afuentes2510@gmail.com>
2021-01-05 17:14:41 +0100

    checking user rol

--- server.js
+++ server.js
@@ -30,14 +30,7 @@ app.get(BASE_API_PATH + "/vehicles", (req, res)  => {
 });
 
 app.get(BASE_API_PATH + "/vehicles/:matricula", (req, res)  => {
-    /*
-    idCliente = req.header('x-user')
-    if (idCliente){
-        toda la peticion
-   }else{
-       res.status(400).send()
-   }*/
-
+    
     Vehicle.findOne({"matricula": req.params.matricula}, (err, vehicle) => {
         if(err){
             console.log(Date()+" - "+ err);
@@ -57,85 +50,117 @@ app.get(BASE_API_PATH + "/vehicles/:matricula", (req, res)  => {
 });
 
 app.post(BASE_API_PATH + "/vehicles", (req, res)  => {
-    var veh = req.body;
-    Vehicle.create(veh, (err) => {
-        if(err)
-        {
-            console.log(Date()+" - "+ err);
-            res.sendStatus(500);
-        }else{
-            console.log(Date() + " POST /vehicles")
-            res.sendStatus(201);
-        }
-    });
+    rolCliente = req.header('rol')
+    if (rolCliente !== "ADMIN"){
+        console.log(Date()+" - Try to post without priviledges");
+        res.status(403 ).send()
+    }
+    else
+    { 
+        var veh = req.body;
+        Vehicle.create(veh, (err) => {
+            if(err)
+            {
+                console.log(Date()+" - "+ err);
+                res.sendStatus(500);
+            }else{
+                console.log(Date() + " POST /vehicles")
+                res.sendStatus(201);
+            }
+        });
+    }
 });
 
 app.put(BASE_API_PATH + "/vehicles/:matricula", (req, res)  => {
-    let matricula = req.params.matricula;
-    let update_vehicle = req.body;
-    Vehicle.findOneAndUpdate({"matricula": matricula}, update_vehicle, { runValidators: true }, (err, vehicle_update) => {
-        if(err == null && vehicle_update == null)
-        {    
-            var auxErr = new Error("Vehicle not found " + matricula);
-            console.log(Date()+" - "+auxErr);
-            res.sendStatus(404)
-        }
-        else if(err)
-        {    
-            console.log(Date()+" - "+err);
-            res.sendStatus(500);
-        }else
-        {
-            console.log(Date() + " PUT /vehicles/" + matricula);
-            res.status(200).send({vehicle : vehicle_update, body: req.body});
-        }                                                                     
-                                                                              
-    });
+    rolCliente = req.header('rol')
+    if (rolCliente !== "ADMIN"){
+        console.log(Date()+" - Try to post without priviledges");
+        res.status(403 ).send()
+    }
+    else
+    { 
+        let matricula = req.params.matricula;
+        let update_vehicle = req.body;
+        Vehicle.findOneAndUpdate({"matricula": matricula}, update_vehicle, { runValidators: true }, (err, vehicle_update) => {
+            if(err == null && vehicle_update == null)
+            {    
+                var auxErr = new Error("Vehicle not found " + matricula);
+                console.log(Date()+" - "+auxErr);
+                res.sendStatus(404)
+            }
+            else if(err)
+            {    
+                console.log(Date()+" - "+err);
+                res.sendStatus(500);
+            }else
+            {
+                console.log(Date() + " PUT /vehicles/" + matricula);
+                res.status(200).send({vehicle : vehicle_update, body: req.body});
+            }                                                                     
+                                                                                
+        });
+    }    
 });
 
  
 app.patch(BASE_API_PATH + "/vehicles/:matricula", (req, res)  => {
-    let matricula = req.params.matricula;
-    Vehicle.findOneAndUpdate({"matricula": matricula}, req.body, { runValidators: true }, (err, vehicle_update) => {
-        if(err == null && vehicle_update == null)
-        {    
-            var auxErr = new Error("Vehicle not found " + matricula);
-            console.log(Date()+" - "+auxErr);
-            res.sendStatus(404)
-        }
-        else if(err)
-        {    
-            console.log(Date()+" - "+err);
-            res.sendStatus(500)
-        }else
-        {
-            console.log(Date() + " PATCH /vehicles/" + matricula);
-            res.status(200).send({vehicle : vehicle_update}); //envia la informacion del viejo no del nuevo
-        }
-    });
+    rolCliente = req.header('rol')
+    if (rolCliente !== "ADMIN"){
+        console.log(Date()+" - Try to post without priviledges");
+        res.status(403 ).send()
+    }
+    else
+    { 
+        let matricula = req.params.matricula;
+        Vehicle.findOneAndUpdate({"matricula": matricula}, req.body, { runValidators: true }, (err, vehicle_update) => {
+            if(err == null && vehicle_update == null)
+            {    
+                var auxErr = new Error("Vehicle not found " + matricula);
+                console.log(Date()+" - "+auxErr);
+                res.sendStatus(404)
+            }
+            else if(err)
+            {    
+                console.log(Date()+" - "+err);
+                res.sendStatus(500)
+            }else
+            {
+                console.log(Date() + " PATCH /vehicles/" + matricula);
+                res.status(200).send({vehicle : vehicle_update}); //envia la informacion del viejo no del nuevo
+            }
+        });
+    }    
 }); 
 
 
 app.delete(BASE_API_PATH + "/vehicles/:matricula", (req, res)  => {
-    let matricula = req.params.matricula;
+    rolCliente = req.header('rol')
+    if (rolCliente !== "ADMIN"){
+        console.log(Date()+" - Try to post without priviledges");
+        res.status(403 ).send()
+    }
+    else
+    { 
+        let matricula = req.params.matricula;
 
-    Vehicle.findOneAndDelete({"matricula": matricula}, (err, vehicle_to_delete) => {
-        if(err == null && vehicle_to_delete == null)
-        {    
-            var auxErr = new Error("Vehicle not found " + matricula);
-            console.log(Date()+" - "+auxErr);
-            res.sendStatus(404)
-        }
-        else if(err)
-        {    
-            console.log(err);
-            res.sendStatus(500)
-        }else
-        {
-            console.log(Date() + " DELETE /vehicles/" + matricula)
-            res.status(204).send({message : "Vehicle " + matricula+ " removed"});
-        }
-    });
+        Vehicle.findOneAndDelete({"matricula": matricula}, (err, vehicle_to_delete) => {
+            if(err == null && vehicle_to_delete == null)
+            {    
+                var auxErr = new Error("Vehicle not found " + matricula);
+                console.log(Date()+" - "+auxErr);
+                res.sendStatus(404)
+            }
+            else if(err)
+            {    
+                console.log(err);
+                res.sendStatus(500)
+            }else
+            {
+                console.log(Date() + " DELETE /vehicles/" + matricula)
+                res.status(204).send({message : "Vehicle " + matricula+ " removed"});
+            }
+        });
+    }    
 });
 
 module.exports =app;
